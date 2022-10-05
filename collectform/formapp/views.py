94dd14ed0a12f22a1f2dec9a8fd2913ab92e14ab
ibraheem82 <ibraheemomikunle82@gmail.com>
2022-10-05 23:40:39 +0100

    logic to perform the crud was created [using class base view].

---
+++ collectform/formapp/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+
+# Create your views here.
+from formapp.models import Candidate
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+# * (C) CREATE
+class Create(CreateView):
+    model = Candidate
+    fields = '__all__'
+    success_url  = reverse_lazy('read')
+
+
+# * (R) READ
+class Read(ListView):
+    model = Candidate
+    queryset = Candidate.objects.all()
+
+
+
+# * (U) UPDATE
+class Update(UpdateView):
+    model = Candidate
+    fields  = '__all__'
+    success_url = reverse_lazy('read')
+    
+
+
+# * (D) DELETE
+class Delete(DeleteView):
+     queryset = Candidate.objects.all()
+     success_url = reverse_lazy('read')
+    
