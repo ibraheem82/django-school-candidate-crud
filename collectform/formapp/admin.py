899e7a6cb822392829295b937b9a41210164a0cb
ibraheem82 <ibraheemomikunle82@gmail.com>
2022-10-06 01:12:07 +0100

    career was removed -> student_class.

--- collectform/formapp/admin.py
+++ collectform/formapp/admin.py
@@ -5,11 +5,11 @@ from .models import Candidate
 
 # * inheriting the ModelAdmin in django
 class CandidateAdmin(admin.ModelAdmin):
-    list_display = ['name', 'phone', 'email', 'gender', 'career']
+    list_display = ['name', 'phone', 'email', 'gender', 'student_class']
     
     search_fields = ['name', 'phone', 'email']
 
-    list_fields = ['gender', 'career']
+    list_fields = ['gender', 'student_class']
     list_per_page = 10
 
 
